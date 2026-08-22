ab2e47d3754a3aa56031ba89aaaaea3eae8ade4c
ZHENG JIN FA <10903131@polimi.it>
2026-08-22 10:12:48 +0200

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        verbose = True
+        output = []
+        hashmap = {}
+        for i in range(0,len(strs)):
+            word = strs[i]
+            sorted_word = ''.join(sorted(word))
+            if hashmap.get(sorted_word) == None:
+                hashmap[sorted_word] = [word]
+            else:
+                hashmap[sorted_word].append(word)
+        for values in hashmap.values():
+            output.append(values)
+        return output
